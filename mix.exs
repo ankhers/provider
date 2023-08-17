f2907a0c14af5e3d178abd932f782ae004727370
Saša Jurić <sasa.juric@gmail.com>
2023-08-17 11:26:02 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -25,8 +25,8 @@ defmodule Provider.MixProject do
       {:boundary, "~> 0.8", runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test]},
       {:ecto, "~> 3.7"},
-      {:ex_doc, "~> 0.25.1", only: :dev},
-      {:dialyxir, "~> 1.1", only: [:dev, :test]}
+      {:ex_doc, "~> 0.25", only: :dev},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
   end
 
